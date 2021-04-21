87c9124a8bffecbed9b950473cc8eca5f8a72acb
Evangeline <evangeline_lim@mymail.sutd.edu.sg>
2021-04-22 03:50:33 +0800

    removed unused imports

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import SingleProfileDisplay from "./SingleProfileDisplay";
 import SearchBox from "./SearchBox";
 
--- client/src/SingleProfileDisplay.js
+++ client/src/SingleProfileDisplay.js
@@ -1,18 +1,13 @@
 const SingleProfileDisplay = ({ singleProfile }) => {
 
-  console.log('heyy')
-  console.log(singleProfile)
-  console.log(singleProfile.key)
-  console.log(singleProfile.key!=="")
-  console.log(singleProfile.value!=="")
     return (
       <div className="single-profile-display">
           {/* {(typeof singleProfile.key !=='undefined')? */}
           {(singleProfile.value !=="")?
           <div>
             <h2>Hello, { singleProfile.key }</h2>
-            You are currently <b>{singleProfile.value=="true" ? 'vaccinated' : 'unvaccinated'}</b>
-            <p>{singleProfile.value=="true" ? 'Thanks for doing your part to keep Singapore safe!' : ''}</p>
+            You are currently <b>{singleProfile.value==="true" ? 'vaccinated' : 'unvaccinated'}</b>
+            <p>{singleProfile.value==="true" ? 'Thanks for doing your part to keep Singapore safe!' : ''}</p>
           </div>:    
           <div>Please enter a valid NRIC</div>
           }
