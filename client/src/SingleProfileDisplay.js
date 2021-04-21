e1fe38128301c971b98cb3f72681132e99825d69
Evangeline <evangeline_lim@mymail.sutd.edu.sg>
2021-04-22 03:54:33 +0800

    rly done

--- client/src/SingleProfileDisplay.js
+++ client/src/SingleProfileDisplay.js
@@ -1,13 +1,15 @@
 const SingleProfileDisplay = ({ singleProfile }) => {
 
+  var vaccinated = singleProfile.value.toLowerCase()==="true" 
+
     return (
       <div className="single-profile-display">
           {/* {(typeof singleProfile.key !=='undefined')? */}
           {(singleProfile.value !=="")?
           <div>
             <h2>Hello, { singleProfile.key }</h2>
-            You are currently <b>{singleProfile.value==="true" ? 'vaccinated' : 'unvaccinated'}</b>
-            <p>{singleProfile.value==="true" ? 'Thanks for doing your part to keep Singapore safe!' : ''}</p>
+            You are currently <b>{vaccinated ? 'vaccinated' : 'unvaccinated'}</b>
+            <p>{vaccinated ? 'Thanks for doing your part to keep Singapore safe!' : ''}</p>
           </div>:    
           <div>Please enter a valid NRIC</div>
           }
